a4d8c761be771d1b2d33a5a151e1706b551eee91
Jeremy Castanza <111897513+jeremycastanza@users.noreply.github.com>
2026-04-23 21:35:44 +0000

    selah 0.2.0

--- Formula/selah.rb
+++ Formula/selah.rb
@@ -1,20 +1,20 @@
 class Selah < Formula
   desc "A terminal Bible reader"
   homepage "https://github.com/jeremycastanza/selah"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-    url "https://api.github.com/repos/jeremycastanza/homebrew-selah-tap/releases/assets/391224868", headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
-    sha256 "de603f620896addd9a564fcb804f6876b5a593756d1ed37217968c11c0be2aab"
+    url "https://api.github.com/repos/jeremycastanza/homebrew-selah-tap/releases/assets/403804899", headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
+    sha256 "f63d1b29f6831063002cc245b6810cfbf6264d1a9da6327742edb0417e705370"
     end
   end
 
   on_linux do
     on_arm do
-    url "https://api.github.com/repos/jeremycastanza/homebrew-selah-tap/releases/assets/391224869", headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
-    sha256 "b81fb64337d2c73ef3737e58054770bb9b218abe6da85b69a46e956877128c24"
+    url "https://api.github.com/repos/jeremycastanza/homebrew-selah-tap/releases/assets/403804903", headers: ["Authorization: token #{ENV["HOMEBREW_GITHUB_API_TOKEN"]}", "Accept: application/octet-stream"]
+    sha256 "e6e3e7a4b19db9e4088270d07c0fba7c5250fe6f358f43e9abfbcde0f4a4d512"
     end
   end
 
